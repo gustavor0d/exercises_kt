c1954c10376849ac20ca7d19ed5784e950de2665
gustavor0d <gustavorodriguesoliveira2013@gmail.com>
2025-03-04 15:07:48 -0300

    Exercício 04

---
+++ src/main/kotlin/exercises/exe04.kt
@@ -0,0 +1,12 @@
+package exercises
+
+fun main() {
+    println("Digite um número inteiro:")
+    var numero01 = readln().toIntOrNull() ?: 0
+    println("Digite outro número inteiro:")
+    var numero02 = readln().toIntOrNull() ?: 0
+
+    var soma = numero01 + numero02
+
+    println("A soma entre $numero01 e $numero02 é igual a $soma.")
+}
