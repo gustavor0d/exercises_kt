9a27a30fe24517b8bccb4f61c9627cdd4bc0f531
gustavor0d <gustavorodriguesoliveira2013@gmail.com>
2025-03-04 14:55:10 -0300

    Exercício 03

---
+++ src/main/kotlin/exercises/exe03.kt
@@ -0,0 +1,11 @@
+package exercises
+
+fun main() {
+    println("Nome do funcionário:")
+    var nomeFuncionario = readln()
+
+    println("Salário:")
+    var salario = readln().toDoubleOrNull() ?: 0
+
+    println("O funcionário $nomeFuncionario tem um salário de R$${"%.2f".format(salario)}.")
+}
